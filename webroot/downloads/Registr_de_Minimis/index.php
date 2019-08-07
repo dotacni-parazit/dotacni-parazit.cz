ec6d7349a02aa9a12bf57bd130978c55db658d7c
Marek Sebera <marek.sebera@gmail.com>
2019-08-07 09:33:10 +0200

    framework updates, versioned index files

---
+++ webroot/downloads/Registr_de_Minimis/index.php
@@ -0,0 +1,17 @@
+<ul>
+<li><a href="RDM_KOMPLET_2017.xlsx">RDM_KOMPLET_2017.xslx</a></li>
+<li><a href="RDM_KOMPLET_2017.csv">RDM_KOMPLET_2017.csv</a></li>
+<li><a href="RDM_KOMPLET_2018.xlsx">RDM_KOMPLET_2018.xslx</a></li>
+<li><a href="RDM_KOMPLET_2018.csv">RDM_KOMPLET_2018.csv</a></li>
+<li><hr/></li>
+<li><a href="RDM_2019_01.xlsx">RDM_2019_01.xslx</a></li>
+<li><a href="RDM_2019_02.xlsx">RDM_2019_02.xslx</a></li>
+<li><a href="RDM_2019_03.xlsx">RDM_2019_03.xslx</a></li>
+<li><a href="RDM_2019_04.xlsx">RDM_2019_04.xslx</a></li>
+<li><a href="RDM_2019_05.xlsx">RDM_2019_05.xslx</a></li>
+<li><a href="RDM_2019_01.csv">RDM_2019_01.csv</a></li>
+<li><a href="RDM_2019_02.csv">RDM_2019_02.csv</a></li>
+<li><a href="RDM_2019_03.csv">RDM_2019_03.csv</a></li>
+<li><a href="RDM_2019_04.csv">RDM_2019_04.csv</a></li>
+<li><a href="RDM_2019_05.csv">RDM_2019_05.csv</a></li>
+</ul>
---
+++ webroot/downloads/index.php
@@ -0,0 +1,36 @@
+<ul>
+    <li><a href="CEDR_7Z">CEDR_7Z/</a></li>
+    <li><a href="Registr_de_Minimis">Registr_de_Minimis/</a></li>
+    <li><a href="cedr.db.dump.sql.gz">cedr.db.dump.sql.gz</a></li>
+    <li><a href="dotinfo.sql.gz">dotinfo.sql.gz</a></li>
+    <li><a href="DotInfo.utf8.csv">DotInfo.utf8.csv</a></li>
+    <li><a href="investicniPobidkyCzechInvest.sql.gz">investicniPobidkyCzechInvest.sql.gz</a></li>
+    <li><a href="investicniPobidky-CzechInvest.xls">investicniPobidky-CzechInvest.xls</a></li>
+    <li><a href="konsolidace.sql.gz">konsolidace.sql.gz</a></li>
+    <li><a href="pap.gz">pap.gz</a></li>
+    <li><a href="pap_ico.sql.gz">pap_ico.sql.gz</a></li>
+    <li><a href="PRV_2014.xls">PRV_2014.xls</a></li>
+    <li><a href="PRV_2015_2.xls">PRV_2015_2.xls</a></li>
+    <li><a href="PRV_2015.xls">PRV_2015.xls</a></li>
+    <li><a href="PRV_2016_2.xls">PRV_2016_2.xls</a></li>
+    <li><a href="PRV_2016.xls">PRV_2016.xls</a></li>
+    <li><a href="PRV_2017.xlsx">PRV_2017.xlsx</a></li>
+    <li><a href="PRV_2018.xls">PRV_2018.xls</a></li>
+    <li><a href="PRV_2014.csv">PRV_2014.csv</a></li>
+    <li><a href="PRV_2015_2.csv">PRV_2015_2.csv</a></li>
+    <li><a href="PRV_2015.csv">PRV_2015.csv</a></li>
+    <li><a href="PRV_2016_2.csv">PRV_2016_2.csv</a></li>
+    <li><a href="PRV_2016.csv">PRV_2016.csv</a></li>
+    <li><a href="PRV_2017.csv">PRV_2017.csv</a></li>
+    <li><a href="PRV_2018.csv">PRV_2018.csv</a></li>
+    <li><a href="strukturalniFondy2007-2013.sql.gz">strukturalniFondy2007-2013.sql.gz</a></li>
+    <li><a href="strukturalniFondy2007-2013.xlsx">strukturalniFondy2007-2013.xlsx</a></li>
+    <li><a href="strukturalniFondy2014-2020.sql.gz">strukturalniFondy2014-2020.sql.gz</a></li>
+    <li><a href="strukturalniFondy2020.csv">strukturalniFondy2020.csv</a></li>
+    <li><a href="SZIF_SPD_2014.csv">SZIF_SPD_2014.csv</a></li>
+    <li><a href="SZIF_SPD_2014.xlsx">SZIF_SPD_2014.xlsx</a></li>
+    <li><a href="SZIF_SPD_2015.csv">SZIF_SPD_2015.csv</a></li>
+    <li><a href="SZIF_SPD_2015.xlsx">SZIF_SPD_2015.xlsx</a></li>
+    <li><a href="SZIF_SPD_2016.csv">SZIF_SPD_2016.csv</a></li>
+    <li><a href="SZIF_SPD_2016.xlsx">SZIF_SPD_2016.xlsx</a></li>
+</ul>
